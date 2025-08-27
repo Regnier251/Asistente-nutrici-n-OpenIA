0f2489d4b1eb542b4d86cad20de32d512f8446f1
Regnier251 <oeyji000021951@educem.mx>
2025-08-27 11:08:59 -0600

    Creación de asistente personal implementando IA

--- app.js
+++ app.js
@@ -22,41 +22,111 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-//Ruta / endpoint / url
-app.post("/api/traducir", async(req,res) => {
-    const { text, targetLang } = req.body;
-    const promptSystem1 = "Eres un traductor profesional.";
-    const promptSystem2 = "Solo puedes responder con una traducción directa del texto que el usuario te envíe."
-                            + "Cualquier otra respuesta o conversación está prohibida.";
-    const promptUser = `Traduce el siguiente texto al ${targetLang}:${text}`;
-    // Llamar al LLM o modurlo de openai
+//Funcion para generar plan de dieta
+const generateDietplan = async (userResponses) => {
 
-    try{
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {role: "system", content: promptSystem1},
-                {role: "system", content: promptSystem2},
-                {role: "user", content: promptUser}
-            ],
-            max_tokens:500,
-            response_format: {type:"text"}
-        });
+    //Crear el prompt(sistema, indiaciones para la ia)
+    const promptSystem = {
+        role: "system",
+        content: `Eres un nutricionista experto en crear planes de alimentación personalizados. Basándote en las respuestas del usuario, 
+        genera un plan de dieta semanal equilibrado y saludable.
+        El usuario solo puede  hacer preguntars relacionadas con la nutrición y dietas.
         
-        const translatedText = completion.choices[0].message.content;
-        
-        return res.status(200).json({translatedText});
-
-    }catch(error){
-        console.log(error)
-        return res.status(500).json({error: "Error al traducir el texto}"});
+        El sistema no responderá a ninguna pregunta que no esté relacionada con la nutrición y dietas.`
     }
 
-});
+    //Crear el prompt del usuario
+    const promptUser = {
+        role: "user",
+        content: `El usuario tiene las siguientes características:
+        - Peso: ${userResponses.peso} kg
+        - Estatura: ${userResponses.altura} cm
+        - Objetivo: ${userResponses.objetivo}
+        - Alergias o restricciones alimentarias: ${userResponses.alergias}
+        - Alimentos que no le gustan: ${userResponses.no_gusta}.
+        
+        Devuelve la dieta en formato table markdown con los siguientes campos: Día, Comida, Almuerzo, Cena, Snacks y Calorias.
+        Y no digas nada más, solo la tabla.`
+    }
+
+    //Llamar a la api de openai
+
+    try {
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [promptSystem, promptUser],
+            max_tokens: 1000,
+            temperature: 0.75,
+        });
+        //Devolver la respuesta generada por openai
+        const response = completion.choices[0].message.content;
+        return response;
+    } catch (error) {
+        console.error("Error al generar el plan de dieta:", error);
+        throw error;
+    }
+};
+
+//Objeto con los datos del usuario
+let userData = {};
+
+//Ruta / endpoint / url
+app.post("/api/nutri-chat", async (req, res) => {
+
+    //Primero se pregunta por el peso(frontend)
+
+    //Recibir datos del usuario
+    const userId = req.body.id;
+    const userMessage = req.body.userMessage;
+    
+    //Generar objeto del usuario si no existe
+    if(!userData[userId]){
+        userData[userId] = {};
+    }
+
+    if(!userData[userId].peso){
+        userData[userId].peso = userMessage;
+
+        return res.json({reply: "¿Cuál es tu estatura (cm)?"});
+    }
+
+    if(!userData[userId].altura){
+        userData[userId].altura = userMessage;
+
+        return res.json({reply: "¿Cuál es tu objetivo? (adelgazar, mantener, ganar músculo)"});
+
+    }
+
+    if(!userData[userId].objetivo){
+        userData[userId].objetivo = userMessage;
+
+        return res.json({reply: "Tienes alguna alergia o restricción alimentaria?"});
+
+    }
+
+    if(!userData[userId].alergias){
+        userData[userId].alergias = userMessage;
+
+        return res.json({reply: "¿Qué alimentos que no te gustan?"});
+
+    }
+
+    if(!userData[userId].no_gusta){
+        userData[userId].no_gusta = userMessage;
+        try {
+            const diet = await generateDietplan(userData[userId]);
+            return res.json({reply: `Aquí esta su dieta 🍎💪🥗 \n${diet}`});
+        } catch (error) {
+            return res.status(500).json({reply: "Hubo un error al generar el plan de dieta. Por favor, intenta nuevamente más tarde."});
+        }
+    }
     
 
-//Servir el backend
-app.listen(PORT), () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-}
+    return res.json({reply: "Tu plan de alimentación semanal es: ... (aquí iría el plan generado por OpenAI)"});
+    
+});
 
+//Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,54 +1,95 @@
-let translateButton = document.querySelector("#translateButton");
+//Seleccionar elementos del DOM
+let userInput = document.querySelector("#inputText");
+let resButton = document.querySelector("#resButton");
+const chatBox = document.querySelector(".chat__messages");
+const userId = "anon-" + Date.now();
 
-translateButton.addEventListener("click", async() => {
+function displayMessage(msgText, sender) {
+  const msgDiv = document.createElement("div");
+  msgDiv.classList.add("chat__message");
+  msgDiv.classList.add(
+    sender === "user" ? "chat__message--user" : "chat__message--bot"
+  );
 
-    let inputText = document.querySelector("#inputText");
+  if (sender == "bot") {
+    msgDiv.classList.add("chat__message--ia");
+  }
+  msgDiv.textContent = msgText;
 
-    //valor a traducir
-    const text = inputText.value.trim();
+  chatBox.appendChild(msgDiv);
 
-    //lenguaje de destino
-    const targetLang = document.querySelector("#targetLang").value;
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
 
-    if(!text) return false;
+async function sendMessage() {
+  const myMessage = userInput.value;
 
-    //Meter el mensaje del usuario a la caja de mensajes
-    const userMessage = document.createElement("div");
-    userMessage.className = "chat__message chat__message--user";
-    userMessage.textContent = text;
+  if (!myMessage) return false;
 
-    const messagesContainer = document.querySelector(".chat__messages");
-    messagesContainer.appendChild(userMessage);
-    messagesContainer.scrolTop = messagesContainer.scrollHeight;
+  userInput.value = "";
 
-    //Petición ajax al backend
+  //Añadir mi mensaje de usuario
+  displayMessage(myMessage, "user");
 
-    try {
-        const response = await fetch("/api/traducir", {
-            method: "POST",
-            headers: {"Content-type":"application/json"},
-            body: JSON.stringify({
-                text,
-                targetLang
-            })
-        });
+  //Crear mensaje de carga
+  displayMessage("Cargando...", "bot");
 
-        const data = await response.json();
+  try {
+    const response = await fetch("http://localhost:3000/api/nutri-chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: userId,
+        userMessage: myMessage,
+      }),
+    });
 
-        //Agregaré el mensaje de la IA al chat
-        const botMessage = document.createElement("div");
-        botMessage.className = "chat__message chat__message--bot";
-        botMessage.textContent = data.translatedText;
-        
-        messagesContainer.appendChild(botMessage);
-        messagesContainer.scrolTop = messagesContainer.scrollHeight;
-
-
-    } catch (error) {
-        console.log("Error:",error); 
+    let data;
+    if (response.headers.get("content-type")?.includes("application/json")) {
+      data = await response.json();
+    } else {
+      throw new Error("Respuesta no es JSON");
     }
 
-    inputText.value = "";
+    const botMessages = chatBox.querySelectorAll(".chat__message--ia");
+    const lastBotMsg = botMessages[botMessages.length - 1];
 
-    //Vasciar el input de tipo texto
-})
+    if (lastBotMsg) {
+
+      //Formaterar tabla
+      if (data.reply.length >= 100) {
+        const md = new markdownit();
+        const htmlContent = md.render(data.reply);
+        lastBotMsg.innerHTML = htmlContent;
+      } else{
+        lastBotMsg.textContent = data.reply;
+      }
+
+      chatBox.scrollTop = chatBox.scrollHeight;
+    } else {
+      displayMessage(data.reply, "bot");
+    }
+  } catch (error) {
+    const botMessages = chatBox.querySelectorAll(".chat__message--ia");
+    const lastBotMsg = botMessages[botMessages.length - 1];
+    if (lastBotMsg) {
+      lastBotMsg.textContent =
+        "Error: No se pudo obtener respuesta. Intenta nuevamente.";
+    } else {
+      displayMessage(
+        "Error: No se pudo obtener respuesta. Intenta nuevamente.",
+        "bot"
+      );
+    }
+    console.error(error);
+  }
+}
+
+function formaterarTable(reply) {}
+
+resButton.addEventListener("click", sendMessage);
+userInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
